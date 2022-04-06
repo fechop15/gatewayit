33b7cf097b45db7cbd51716b2f631cb52f8075ca
fredy.perez <fredy.perez@pragma.com.co>
2022-04-06 00:37:28 -0500

    add gateway ti in footer

--- components/footer.js
+++ components/footer.js
@@ -5,6 +5,9 @@ export default function Footer() {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
+        <div className="pt-6 flex flex-col items-center">
+          <h2>Gatewway it</h2>
+        </div>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             Statically Generated with Next.js.
